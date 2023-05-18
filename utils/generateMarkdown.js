fe8d871cfd02ee574098abe5b59b7732bc07c8da
Devin Nunez <dnunez@gibbswire.com>
2023-05-18 09:34:41 -0400

    fully built project

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -67,7 +67,7 @@ function generateMarkdown(data) {
    - [Usage](#Usage)<br/>
    - [Licenses](#Licenses)<br/>
    - [Contributions Guidelines](#Contributions-guidelines)<br/>
-   - [Tests Instrictions](#Tests-Instructions)<br/>
+   - [Tests Instructions](#Tests-Instructions)<br/>
    - [How to reach for questions](#How-to-reach-for-questions)<br/>
 
 
